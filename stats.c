e9f40902fd986474e7bf46fecc7582479992733c
Drew Roberds <drew.roberds@gmail.com>
2024-10-14 13:24:39 -0700

    Completed function definitions in stats.c

--- stats.c
+++ stats.c
@@ -48,35 +48,103 @@ void main() {
 
 void print_array(unsigned char* array, int length)
 {
-
+  printf("[ ");
+  for (int i = 0 ; i < length ; i++)
+      {
+        if((i % 8 == 0) && (i != 0))
+        {
+          printf("\n");
+        }
+        printf("%3d ", array[i]);
+      }
+      printf("]\n\n");
 }
 
 void print_statistics(unsigned char* array, int length)
 {
+  unsigned char median;
+  unsigned char mean;
+  unsigned char max;
+  unsigned char min;
 
+  median = find_median(array, length);
+  mean = find_mean(array, length);
+  max = find_maximum(array, length);
+  min = find_minimum(array, length);
+
+  printf("----------ARRAY STATISTICS REPORT----------\n\n\tMedian = %3d\n\tMean = %3d\n\tMaximum = %3d\n\tMinimum = %3d\n", median, mean, max, min);
 }
 
 unsigned char find_median(unsigned char* array, int length)
 {
+  unsigned char median;
+  sort_array(array, length);
+  if(length % 2 == 0)
+  {
+    median = (array[length/2] + array[(length/2) - 1])/2;
+  }
+  else {median = array[(length - 1)/2];}
 
+  return median;
 }
 
 unsigned char find_mean(unsigned char* array, int length)
 {
-
+  unsigned char mean;
+  int total = 0;
+  for(int i = 0; i < length; i++)
+  {
+    total = total + array[i];
+  }
+  mean = total/length;
+  return mean;
 }
 
 unsigned char find_maximum(unsigned char* array, int length)
 {
-
+  unsigned char max = 0;
+  for (int i = 0; i < length ; i++)
+  {
+    if(array[i] > max)
+    {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 unsigned char find_minimum(unsigned char* array, int length)
 {
-
+  unsigned char min = 0xFF;
+  for (int i = 0; i < length ; i++)
+  {
+    if(array[i] < min)
+    {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 void sort_array(unsigned char* array, int length)
 {
+  int max;        //used to hold current max value
+  int replace;    //used to hold index of value to replace in array
+  int index = 0;  //used to index to end of array after each iteration
 
+  while(length - index != 0)
+  {
+    max = 0;
+    for(int i = index ; i < length ; i++)
+    {
+      if(array[i] > max)
+      {
+        max = array[i];
+        replace = i;
+      }
+    }
+    array[replace] = array[index];
+    array[index] = max;
+    index++;
+  }
 }
